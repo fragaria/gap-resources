fb110699e05574f297771790616947fafced57db
Robin Gottfried <czervenka@gmail.com>
2013-10-17 13:26:29 +0200

    Discovery now accepts model resources and models besides `models` module

--- resources/discovery.py
+++ resources/discovery.py
@@ -3,12 +3,20 @@ from google.appengine.ext import ndb
 from gap.utils.imports import import_class
 
 from register import register
+from resources.resource import Resource
 
 
 def discover_models(modules):
     for module in modules:
         module = import_class(module)
 
-        for model in [m for m in module.__dict__.values() if isinstance(m, ndb.model.MetaModel)]:
-            register(model)
+        if module.__class__ == 'module':
+            for model in [m for m in module.__dict__.values() if isinstance(m, ndb.model.MetaModel)]:
+                register(model)
+        elif isinstance(module, ndb.model.MetaModel):  # the module is actualy a model
+            register(module)
+        elif issubclass(module, Resource):
+            register(module.model, module)
+        else:
+            raise TypeError("Expected modul, resource or model but got %s.%s" % (module.__module__, module.__class__.__name__))
 
